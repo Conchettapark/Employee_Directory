c744c0a39482c1495b4e5883736fed01e31c2861
Conchetta Park <conchettapark@MacBook-Pro.local>
2020-10-06 18:32:49 -0400

    Updates

---
+++ precache-manifest.06183c2bda2889ba5ce038b2836ffb34.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d4a7fffa7ef7d0816ed17da3d45d03f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6d12e97ab7d698e9090c",
+    "url": "/static/css/main.87223bac.chunk.css"
+  },
+  {
+    "revision": "d5ab0b1a14559313f36c",
+    "url": "/static/js/2.455a8692.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.455a8692.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d12e97ab7d698e9090c",
+    "url": "/static/js/main.a2713132.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
